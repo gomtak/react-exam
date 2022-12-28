2985f324bc38109a091c8f5964416757130d86db
김경탁 <tak6831@gmail.com>
2022-12-28 19:01:06 +0900

    tesmp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
+
+var i = 0;
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+  console.log("i run all the time");
+  console.log(i++);
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only time");
+  // }
+  // useEffect(iRunOnlyOnce, [])
   return (
     <div>
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
