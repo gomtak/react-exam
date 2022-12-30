d5629f97eb7040ef862dd68f0231f988a34d4830
김경탁 <tak6831@gmail.com>
2022-12-30 18:21:14 +0900

    init

--- src/App.js
+++ src/App.js
@@ -2,29 +2,8 @@ import { useState, useEffect } from "react";
 
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyworkd] = useState("");
-
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => setKeyworkd(event.target.value);
-
-  useEffect(() => {
-    console.log("i run only once");
-  }, [])
-  useEffect(() => {
-    console.log("I run when 'keyword change", keyword);
-  }, [keyword])
-  useEffect(() => {
-    console.log("I run when 'counter change", counter);
-  }, [counter])
-  useEffect(() => {
-    console.log("I run when 'keyword and counter change");
-  }, [counter, keyword])
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
     </div>
   );
 }
