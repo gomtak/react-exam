bd354e10eded681b4d244f997118ac6ee3f7b522
김경탁 <tak6831@gmail.com>
2022-12-30 14:16:15 +0900

    React useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import { useState, useEffect } from "react";
 
-var i = 0;
+
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyworkd] = useState("");
+
   const onClick = () => setValue((prev) => prev + 1)
-  console.log("i run all the time");
-  console.log(i++);
-  // const iRunOnlyOnce = () => {
-  //   console.log("i run only time");
-  // }
-  // useEffect(iRunOnlyOnce, [])
+  const onChange = (event) => setKeyworkd(event.target.value);
+
+  useEffect(() => {
+    console.log("i run only once");
+  }, [])
+  useEffect(() => {
+    console.log("I run when 'keyword change", keyword);
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run when 'counter change", counter);
+  }, [counter])
+  useEffect(() => {
+    console.log("I run when 'keyword and counter change");
+  }, [counter, keyword])
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
